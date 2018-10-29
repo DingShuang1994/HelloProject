67e955ba63f42c3bc06ef3bb51dd3c7c6087ccf6
Mr.ding <dingshuangshuang@163.com>
2018-10-29 16:05:28 +0800

    使用master分支修改后提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -8,4 +8,7 @@ public class Main {
         System.out.println("修改和添加到本地仓库后 推送到github远程仓库中");
         System.out.println("clone下来的工程 可以直接push到远程仓库中相当于同步 不过要用https协议的才行");
     }
+    public static void masterBranch(){
+        System.out.println("master branch master分支中的内容更改");
+    }
 }
